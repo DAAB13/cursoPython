08b61647616ef28bb43a629e997770792b8fc31f
DAAB13 <diego.ab130@gmail.com>
2025-07-23 22:33:48 -0500

    Resuelvo conflicto por cambio de nombre de archivo

---
+++ 02_flow_control/04.1_ejecticios.py
@@ -0,0 +1,25 @@
+# Ejercicio 1: Añadir y modificar elementos
+# Crea una lista con los números del 1 al 5.
+# Añade el número 6 al final usando append().
+# Inserta el número 10 en la posición 2 usando insert().
+# Modifica el primer elemento de la lista para que sea 0.
+"""
+lista_1 = [1, 2, 3, 4, 5]
+lista_1.append(6)
+print(lista_1)
+
+lista_1.insert(1, 10)
+print(lista_1)
+
+lista_1[0] = 0
+print(lista_1)
+"""
+
+# Ejercicio 2: Combinar y limpiar listas
+# Crea dos listas:
+# lista_a = [1, 2, 3]
+# lista_b = [4, 5, 6, 1, 2]
+# Extiende lista_a con lista_b usando extend().
+# Elimina la primera aparición del número 1 en lista_a usando remove().
+# Elimina el elemento en el índice 3 de lista_a usando pop(). Imprime el elemento eliminado.
+# Limpia completamente lista_b usando clear().
